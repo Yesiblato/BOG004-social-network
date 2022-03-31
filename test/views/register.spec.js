3b51a8f48c56ede400a3157f6556a32280da427e
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2022-03-31 15:10:06 -0500

    Test ArrowBack

--- test/views/register.spec.js
+++ test/views/register.spec.js
@@ -1,4 +1,5 @@
 import { registerPage } from '../../src/lib/views/register.js';
+import { showChange } from '../../src/lib/router.js';
 
 jest.mock('../../src/lib/firebase/firebase-imports.js');
 
@@ -21,5 +22,35 @@ describe('registerPage', () => {
     btn.dispatchEvent(new Event('click'));
 
     expect(noteError.textContent).toBe('Todos los campos son obligatorios');
+  });
+});
+
+describe('arrow back ', () => {
+  it('comprueba que nos regresa a la pagina de login', () => {
+    const result = registerPage();
+    const back = result.querySelector('.back');
+    document.body.innerHTML = '<section id="showPages"> </section>';
+    const mainContainer = document.querySelector('#showPages');
+    back.dispatchEvent(new Event('click'));
+    const htmlTemplate = `
+    <div class="containerPrincipal">.
+    <div id="container-login">
+      <div class="mediaLogin">
+        <img alt="logo" src="img/logo.png" />
+        <h2 id="title1">Latam</h2>
+        <h2 id="title2">Sin Fronteras</h2>
+      </div>
+      <form id="login-form">
+        <input id="mail" placeholder="Correo" type="text" />
+        <input id="password" placeholder="Contraseña" type="password" />
+        <p id="containerErrorP"> </p>
+        <button class="btn-inciarSesion">iniciar Sesion</button>
+        <button class="btn-google">iniciar Sesion con google</button>
+        <a href="#/registrate"> ¿No tienes cuenta? Registrate </a>
+      </form>
+    </div>
+    </div>
+    `;
+    expect(showChange('').innerHTML).toEqual(htmlTemplate);
   });
 });
